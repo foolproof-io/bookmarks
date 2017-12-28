391b3119eca1333d1661a4f4f3d36c793eb39dc2
Ryan P. Brewster <RyanPBrewster@gmail.com>
2017-12-28 07:56:04 -0800

    Fix inline script evaluation issue

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,25 @@
-var db = firebase.firestore();
-
-chrome.browserAction.onClicked.addListener((tab) => {
-  db.collection("keeps").add({
-    url: tab.url,
-    title: tab.title,
-    keptAt: firebase.firestore.FieldValue.serverTimestamp()
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  var config = {
+    apiKey: "AIzaSyA2iCPPGb7_Bb3oAUTXsnOfbUeszyIdyYI",
+    authDomain: "foolproof-bookmarks.firebaseapp.com",
+    databaseURL: "https://foolproof-bookmarks.firebaseio.com",
+    projectId: "foolproof-bookmarks",
+    storageBucket: "foolproof-bookmarks.appspot.com",
+    messagingSenderId: "518485674389"
+  };
+  firebase.initializeApp(config);
+  main();
 });
+
+
+function main() {
+  var db = firebase.firestore();
+
+  chrome.browserAction.onClicked.addListener((tab) => {
+    db.collection("keeps").add({
+      url: tab.url,
+      title: tab.title,
+      keptAt: firebase.firestore.FieldValue.serverTimestamp()
+    });
+  });
+}

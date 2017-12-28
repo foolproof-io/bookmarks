56958876186da20649acfc1c7bc4f305a316e99b
Ryan P. Brewster <RyanPBrewster@gmail.com>
2017-12-28 08:19:32 -0800

    Half-hearted attempt to get auth working

--- extension/background.js
+++ extension/background.js
@@ -1,25 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var config = {
-    apiKey: "AIzaSyA2iCPPGb7_Bb3oAUTXsnOfbUeszyIdyYI",
-    authDomain: "foolproof-bookmarks.firebaseapp.com",
-    databaseURL: "https://foolproof-bookmarks.firebaseio.com",
-    projectId: "foolproof-bookmarks",
-    storageBucket: "foolproof-bookmarks.appspot.com",
-    messagingSenderId: "518485674389"
-  };
-  firebase.initializeApp(config);
-  main();
-});
+var config = {
+  apiKey: "AIzaSyA2iCPPGb7_Bb3oAUTXsnOfbUeszyIdyYI",
+  authDomain: "foolproof-bookmarks.firebaseapp.com",
+  databaseURL: "https://foolproof-bookmarks.firebaseio.com",
+  projectId: "foolproof-bookmarks",
+  storageBucket: "foolproof-bookmarks.appspot.com",
+  messagingSenderId: "518485674389"
+};
+firebase.initializeApp(config);
 
+chrome.browserAction.onClicked.addListener(internKeep);
 
-function main() {
-  var db = firebase.firestore();
+function internKeep(tab) {
+  if (!firebase.auth().currentUser) {
+    return forceAuth();
+  }
+  return firebase.firestore().collection("keeps").add({
+    url: tab.url,
+    title: tab.title,
+    keptBy: firebase.auth().currentUser.uid,
+    keptAt: firebase.firestore.FieldValue.serverTimestamp()
+  });
+}
 
-  chrome.browserAction.onClicked.addListener((tab) => {
-    db.collection("keeps").add({
-      url: tab.url,
-      title: tab.title,
-      keptAt: firebase.firestore.FieldValue.serverTimestamp()
-    });
+function forceAuth() {
+  console.log("starting auth process");
+  chrome.identity.getAuthToken({interactive: false}, token => {
+    if(chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+    } else if (!token) {
+      console.error('The OAuth Token was null');
+    } else {
+      console.log("got a token: ", token);
+      // Authrorize Firebase with the OAuth Access Token.
+      var credential = firebase.auth.GoogleAuthProvider.credential(null, token);
+      console.log("credential = " + credential);
+      firebase.auth().signInWithCredential(credential).catch(error => {
+        // The OAuth token might have been invalidated. Lets' remove it from cache.
+        if (error.code === 'auth/invalid-credential') {
+          console.log("credential was invalid, starting over");
+          chrome.identity.removeCachedAuthToken({token: token}, function() {
+            forceAuth();
+          });
+        }
+      });
+    }
   });
 }
